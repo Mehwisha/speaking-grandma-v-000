b33d39b73f3d2927b3d406fd57b0f53660807441
Mehwish Aziz <mehwisha@msn.com>
2018-05-01 00:55:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
    "I LOVE YOU TOO PUMPKIN!"
  elsif phrase == phrase.downcase
   		return "HUH?! SPEAK UP, SONNY!"
-    elsif
+    elsif phrase == phrase.upcase
   	return	"NO, NOT SINCE 1938!"
     end
   # if speak == speak.upcase
